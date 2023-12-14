1193483a0d9a3b1e5a672242987f60d59e06fa3c
DomenicoScala <dome.scala12@gmail.com>
2023-12-14 18:46:33 +0100

    BONUS COMPLETATO

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
 const myButton = document.getElementById('special-button')
 
+const diffSelect = document.getElementById('difficoltà')
+
+
+
 myButton.addEventListener('click', function(){
     console.log('myButton', myButton, typeof myButton)
     const gridContainer = document.querySelector('.result-grid')
 
-    for(let i = 1; i <= 100; i++){
+    const cellNumber = parseInt(diffSelect.value)
+
+    gridContainer.innerHTML = '';
+
+    for(let i = 1; i <= cellNumber; i++){
         const cell = document.createElement('div')
-        cell.classList.add('cell')
+        cell.classList.add('cell', 'cell-' + cellNumber)
         cell.innerHTML += i
         gridContainer.append(cell)
         
